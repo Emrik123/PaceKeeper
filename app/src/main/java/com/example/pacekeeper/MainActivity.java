66e20c12f2385a08d9127b21154c8af8ba56502a
Munther23 <jonathan.munther@gmail.com>
2024-03-25 21:33:03 +0100

    Functional runnerview

--- app/src/main/java/com/example/pacekeeper/MainActivity.java
+++ app/src/main/java/com/example/pacekeeper/MainActivity.java
@@ -11,31 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.google.android.gms.location.*;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText speedInput;
-    private TextView speedDisplay;
-    private TextView statusDisplay;
-    private TextView distanceDisplay;
+    private int speed;
     private Button confirm;
-    private com.google.android.gms.location.LocationRequest locationRequest;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private LocationCallback locationCallback;
-    private Location location;
-    private double currentSpeed;
-    private double speed;
-    private double distance;
     private Vibrator vibrator;
-    private VibrationEffect increaseSpeedVibrationPattern = VibrationEffect.createWaveform(new long[]{150,75,150,75,150},new int[]{255,0,255,0,255},-1); //Creates Vibration pattern for being too slow
-    private VibrationEffect decreaseSpeedVibrationPattern = VibrationEffect.createWaveform(new long[]{900},new int[]{255},-1); //Creates Vibration pattern for being too fast
+    private VibrationEffect increaseSpeedVibrationPattern = VibrationEffect.createWaveform(new long[]{150, 75, 150, 75, 150}, new int[]{255, 0, 255, 0, 255}, -1); //Creates Vibration pattern for being too slow
+    private VibrationEffect decreaseSpeedVibrationPattern = VibrationEffect.createWaveform(new long[]{900}, new int[]{255}, -1); //Creates Vibration pattern for being too fast
 
 
     @SuppressLint("WrongViewCast")
@@ -43,85 +33,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        currentSpeed = 0;
-        speedInput = findViewById(R.id.leftNPicker);
         confirm = findViewById(R.id.confirmButton);
-       // speedDisplay = findViewById(R.id.speedDisplay);
-       // statusDisplay = findViewById(R.id.statusDisplay);
-       // distanceDisplay = findViewById(R.id.distanceDisplay);
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(500);
-        locationRequest.setFastestInterval(500);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-        vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE); //Assigns vibrator to local variable
+        NumberPicker numberPicker = findViewById(R.id.leftNPicker);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(40);
+
+
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String input = speedInput.getText().toString();
-                if(!input.isEmpty()){
-                    speed = Double.parseDouble(input);
-                    Toast.makeText(MainActivity.this, "Speed stored." , Toast.LENGTH_SHORT).show();
-                    start();
-                }else{
-                    Toast.makeText(MainActivity.this, "Please enter a valid speed." , Toast.LENGTH_SHORT).show();
+                speed = numberPicker.getValue();
+                if (speed != 0) {
+                    Toast.makeText(MainActivity.this, "Speed stored.", Toast.LENGTH_SHORT).show();
+                    displayRunnerView(speed);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a valid speed.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        locationCallback = new LocationCallback(){
-
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                if(location != null){
-                    distance = distance + location.distanceTo(locationResult.getLastLocation());
-                }
-                location = locationResult.getLastLocation();
-                currentSpeed = location.getSpeed();
-                updateUI();
-            }
-        };
     }
 
-    private void start() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            BlankFragment blankFragment = new BlankFragment();
-            return;
 
-        }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    private void displayRunnerView(int speed) {
+        // Create a new instance of RunnerView fragment with the selected speed
+        RunnerView runnerView = RunnerView.newInstance(speed);
+
+        // Replace the current fragment with the RunnerView fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, runnerView); // Replace fragment_container with the id of your container layout
+        transaction.addToBackStack(null); // Optional: Add transaction to back stack
+        transaction.commit();
     }
 
-    @SuppressLint("SetTextI18n")
-    public void updateUI(){
-        int roundedDistance = (int) distance;
-        distanceDisplay.setText(Integer.toString(roundedDistance));
-        currentSpeed = currentSpeed * 3.6;
-        int roundedSpeed = (int) currentSpeed;
-        String s1 = Double.toString(roundedSpeed);
-        String s2 = getString(R.string.viewString);
-        String s3 = getString(R.string.viewString2);
-        speedDisplay.setText(s1);
-        //viewSpeed.setText(s2 + s1 + s3);
-        //Toast.makeText(MainActivity.this,Double.toString(currentSpeed), Toast.LENGTH_SHORT).show();
-        if(roundedSpeed == speed || (roundedSpeed >= speed -1 && roundedSpeed <= speed +1)){
-            statusDisplay.setText(getString(R.string.reachedSpeed));
-            speedDisplay.setTextColor(Color.parseColor("green"));
-        }else if(roundedSpeed > speed+1){
-            statusDisplay.setText(getString(R.string.tooFast));
-            speedDisplay.setTextColor(Color.parseColor("red"));
-            vibrator.vibrate(decreaseSpeedVibrationPattern); //Calls for the vibrator to vibrate according to the decreaseSpeedVibrationPattern
-        }else if(roundedSpeed < speed-1){
-            statusDisplay.setText(getString(R.string.tooSlow));
-            speedDisplay.setTextColor(Color.parseColor("blue"));
-            vibrator.vibrate(increaseSpeedVibrationPattern); //Calls for the vibrator to vibrate according to the increaseSpeedVibrationPattern
-        }
-    }
-    public void scroll(){
-      
-        
-
-    }
 }
---
+++ app/src/main/java/com/example/pacekeeper/RunnerView.java
@@ -0,0 +1,130 @@
+package com.example.pacekeeper;
+
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RunnerView#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RunnerView extends Fragment {
+    private NumberPicker speedInput;
+    private TextView speedDisplay;
+    private TextView statusDisplay;
+    private TextView distanceDisplay;
+    private Button confirm;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private Location location;
+    private double currentSpeed;
+    private double speed;
+    private double distance;
+
+    public RunnerView() {
+        // Required empty public constructor
+    }
+
+    public static RunnerView newInstance(int speed) {
+        RunnerView fragment = new RunnerView();
+        Bundle args = new Bundle();
+        // You can pass arguments if needed
+        args.putInt("speed", speed);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        // Initialize views
+        speedDisplay = rootView.findViewById(R.id.speedDisplay);
+        distanceDisplay = rootView.findViewById(R.id.distanceDisplay);
+
+        // Initialize speed with the value passed through arguments
+        Bundle args = getArguments();
+        if (args != null) {
+            speed = args.getInt("speed", 0);
+        }
+
+        // Setup location request and provider client
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(500);
+        locationRequest.setFastestInterval(500);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
+
+        // Setup location callback
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (location != null) {
+                    distance = distance + location.distanceTo(locationResult.getLastLocation());
+                }
+                location = locationResult.getLastLocation();
+                currentSpeed = location.getSpeed();
+                updateUI();
+            }
+        };
+        start();
+
+        return rootView;
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void updateUI(){
+        int roundedDistance = (int) distance;
+        distanceDisplay.setText(Integer.toString(roundedDistance));
+        currentSpeed = currentSpeed * 3.6;
+        int roundedSpeed = (int) currentSpeed;
+        String s1 = Double.toString(roundedSpeed);
+        String s2 = getString(R.string.viewString);
+        String s3 = getString(R.string.viewString2);
+        speedDisplay.setText(s1);
+
+
+        if(roundedSpeed == speed || (roundedSpeed >= speed -1 && roundedSpeed <= speed +1)){
+            speedDisplay.setTextColor(Color.parseColor("green"));
+        }else if(roundedSpeed > speed+1){
+            speedDisplay.setTextColor(Color.parseColor("red"));
+        }else if(roundedSpeed < speed-1){
+            speedDisplay.setTextColor(Color.parseColor("blue"));
+        }
+    }
+
+
+
+
+
+    private void start() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+}
