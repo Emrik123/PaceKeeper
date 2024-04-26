aedf5c63f53a4318c04efb15b669d48f5c99ff25
Samuel Lundman <samuelgustavlundman@gmail.com>
2024-04-26 15:06:40 +0200

    Added dependencies and permissions in manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation ("com.google.android.gms:play-services-location:21.2.0")
     implementation ("org.apache.commons:commons-math3:3.6.1")
     implementation ("org.apache.commons:commons-lang3:3.14.0")
+    implementation("androidx.core:core:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
