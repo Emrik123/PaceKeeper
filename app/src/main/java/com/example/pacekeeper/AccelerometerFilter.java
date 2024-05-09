3af33ac3d38a087b0be9424127ecaa1f82d470e4
Samuel Lundman <samuelgustavlundman@gmail.com>
2024-05-09 16:50:47 +0200

    Small corrections to skeleton

--- app/src/main/java/com/example/pacekeeper/AccelerometerFilter.java
+++ app/src/main/java/com/example/pacekeeper/AccelerometerFilter.java
@@ -29,20 +29,22 @@ public class AccelerometerFilter {
         //R = MatrixUtils.createRealMatrix(new double[][]{noiseVariance});
         H = MatrixUtils.createRowRealMatrix(new double[]{1, 0});
         P = MatrixUtils.createRealIdentityMatrix(2);
-
     }
 
     public void recalculateMatrices() {
         // Recalculate A, B and Q with updated dt
     }
 
-    public void predict(double acc) {
-
+    public void predict() {
+        x = A.operate(x);
+        P = A.multiply(P).multiply(A.transpose()).add(Q);
     }
 
-    public void update(double dt) {
+    public void update(double acc, double dt) {
         this.dt = dt;
         recalculateMatrices();
+
+
     }
 
     public double[] getState() {
