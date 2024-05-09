0ac7c7327b11fbe60942f7a7c6c07daac9070655
Samuel Lundman <samuelgustavlundman@gmail.com>
2024-05-09 16:12:15 +0200

    Skeleton for Kalmanfilter

---
+++ app/src/main/java/com/example/pacekeeper/AccelerometerFilter.java
@@ -0,0 +1,51 @@
+package com.example.pacekeeper;
+
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
+public class AccelerometerFilter {
+    private RealMatrix A, B, Q, R, H, K, P;
+    private RealVector x;
+    private double dt;
+
+    public AccelerometerFilter() {
+        dt = 0.5; // Initial value, will be recalculated
+        initMatrices();
+    }
+
+    public void initMatrices() {
+        A = MatrixUtils.createRealMatrix(new double[][]{
+                {1, dt},
+                {0, 1}
+        });
+
+        //B = MatrixUtils.createRealMatrix(new double[][]{})
+
+        //double std; //standard covariance
+        //Q = MatrixUtils.createRealMatrix(new double[][]{}).scalarMultiply(std);
+
+        //double noiseVariance;
+        //R = MatrixUtils.createRealMatrix(new double[][]{noiseVariance});
+        H = MatrixUtils.createRowRealMatrix(new double[]{1, 0});
+        P = MatrixUtils.createRealIdentityMatrix(2);
+
+    }
+
+    public void recalculateMatrices() {
+        // Recalculate A, B and Q with updated dt
+    }
+
+    public void predict(double acc) {
+
+    }
+
+    public void update(double dt) {
+        this.dt = dt;
+        recalculateMatrices();
+    }
+
+    public double[] getState() {
+        return x.toArray();
+    }
+}
