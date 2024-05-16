09d097e1256abd91a17fb8c234b09217ff105199
Samuel Lundman <samuelgustavlundman@gmail.com>
2024-05-16 09:22:54 +0200

    Filter now accounts for that accelerometer readings might vary over time

--- app/src/main/java/com/example/pacekeeper/AccelerometerFilter.java
+++ app/src/main/java/com/example/pacekeeper/AccelerometerFilter.java
@@ -9,7 +9,9 @@ public class AccelerometerFilter {
     private RealMatrix A, Q, R, H, K, P;
     private RealVector x;
     private double dt;
-    private final double variance = 0.15;
+    //private final double variance = 0.15;
+    private final double initialVariance = 0.15;
+    private double processNoiseFactor = 0.5;
 
     public AccelerometerFilter() {
         dt = 0.5; // Initial value, will be recalculated
@@ -23,16 +25,29 @@ public class AccelerometerFilter {
         });
 
         // Q is replaced with a scalar, essentially assuming a constant process noise variance across all state variables.
-        Q = MatrixUtils.createRealMatrix(new double[][]{
+        /*Q = MatrixUtils.createRealMatrix(new double[][]{
                 {1, dt},
-                {0, 1}}).scalarMultiply(variance);
+                {0, 1}}).scalarMultiply(variance);*/
 
-        double noiseVariance = 0.9;
-        R = MatrixUtils.createRealMatrix(new double[][]{{noiseVariance}});
+        // Adaptive Q
+        /*Q = MatrixUtils.createRealMatrix(new double[][]{
+                {0.25 * dt * dt * dt * dt, 0.5 * dt * dt * dt},
+                {0.5 * dt * dt * dt, dt * dt}}).scalarMultiply(initialVariance);*/
+
+        Q = calculateProcessNoiseCovariance(dt, initialVariance);
+
+        double measurementNoiseVariance = 0.1;
+        R = MatrixUtils.createRealMatrix(new double[][]{{measurementNoiseVariance}});
 
         x = new ArrayRealVector(new double[]{0, 0});
         H = MatrixUtils.createRowRealMatrix(new double[]{1, 0});
         P = MatrixUtils.createRealIdentityMatrix(2);
+    }
+
+    private RealMatrix calculateProcessNoiseCovariance(double dt, double variance) {
+        return MatrixUtils.createRealMatrix(new double[][]{
+                {dt * dt, dt},
+                {dt, 1}}).scalarMultiply(variance * processNoiseFactor);
     }
 
     private void recalculateMatrices() {
@@ -42,9 +57,15 @@ public class AccelerometerFilter {
                 {0, 1}
         });
 
-        Q = MatrixUtils.createRealMatrix(new double[][]{
+        /*Q = MatrixUtils.createRealMatrix(new double[][]{
                 {1, dt},
-                {0, 1}}).scalarMultiply(variance);
+                {0, 1}}).scalarMultiply(variance);*/
+
+        /*Q = MatrixUtils.createRealMatrix(new double[][]{
+                {0.25 * dt * dt * dt * dt, 0.5 * dt * dt * dt},
+                {0.5 * dt * dt * dt, dt * dt}}).scalarMultiply(initialVariance * processNoiseFactor);*/
+
+        Q = calculateProcessNoiseCovariance(dt, initialVariance);
     }
 
     private void predict() {
@@ -58,8 +79,17 @@ public class AccelerometerFilter {
         RealMatrix S = H.multiply(P).multiply(H.transpose()).add(R);
         K = P.multiply(H.transpose()).multiply(MatrixUtils.inverse(S));
 
+        RealVector innovation = accVector.subtract(H.operate(x));
         x = x.add(K.operate(accVector.subtract(H.operate(x))));
         P = P.subtract(K.multiply(H).multiply(P));
+
+        // Dynamic weighting of process noise (Q)?
+        double innovationNorm = innovation.getNorm();
+        if (innovationNorm > Math.sqrt(R.getEntry(0, 0))) {
+            processNoiseFactor *= 1.1; // Increase process noise
+        } else {
+            processNoiseFactor *= 0.9; // Decrease process noise
+        }
     }
 
     public void update(double acc, double dt) {
